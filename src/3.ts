3eca4b1d559397d631b079fd3a095e3c6e707e23
Arnary <anastasia.dushka1@gmail.com>
2024-06-30 20:26:13 +0300

    add hw

--- src/3.ts
+++ src/3.ts
@@ -3,11 +3,21 @@
   Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
 
   Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
+  Інтерфейс department повинен включати властивості name і level, і навіть метод introduce і levelUp. 
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
-// реалізація класу Wizard
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+};
+
+interface ISpellCaster {
+  castSpell(): void;
+};
+
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -27,9 +37,8 @@ class Wizard implements ICharacter, ISpellCaster {
     this.level++;
     console.log(`Level up! New level is ${this.level}`);
   }
-}
+};
 
-// тестування класу
 const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature() {
+        return this.signature;
+     }
+};
+
+class Person {
+    constructor(private key: Key) {
+    };
+
+    getKey() {
+        return this.key;
+    }
+};
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+};
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+
+    openDoor(key: Key): void {
+        if (key.getSignature === this.key.getSignature) {
+            this.door = true;
+        }
+    }
+};
+
 const key = new Key();
 
 const house = new MyHouse(key);
